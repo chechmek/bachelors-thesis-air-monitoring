7aab7f20d81d02a6b7e8528b994d266419b6894d
roman.babukh <roman.babukh@rsrg.com>
2025-05-24 21:24:11 +0200

    Update About page content with Bachelors Thesis details

--- src/sensor-data-web-app/src/app/about/page.tsx
+++ src/sensor-data-web-app/src/app/about/page.tsx
@@ -5,7 +5,12 @@ export default function About() {
         <h1 className="text-3xl font-bold mb-8 text-gray-900">About</h1>
         <div className="bg-white rounded-lg shadow-lg p-6">
           <p className="text-lg leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Bachelors Thesis of Roman Babukh in Kyiv Polytechnic Institute
+          </p>
+          <br />
+          <p className="text-lg leading-relaxed text-gray-700"> Topic: </p>
+          <p className="text-lg leading-relaxed text-gray-700">
+            Robotic Air Environment Monitoring System with Analysis and Alerting Function
           </p>
         </div>
       </div>
